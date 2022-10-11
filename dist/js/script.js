7685f82ad9102434474f7e82b864204e7ded84cf
Agnieszka Wenerska <wenerskaaga@gmail.com>
2022-10-11 18:01:30 +0200

    Add processOrder

--- dist/js/script.js
+++ dist/js/script.js
@@ -59,7 +59,10 @@
       thisProduct.data = data;
 
       thisProduct.renderIdMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('new Product:', thisProduct);
     }
@@ -79,6 +82,22 @@
     /* initAccordion(){
       const thisProduct = this;
     } */
+
+    getElements(){
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /* console.log('this product trigger', thisProduct.accordionTrigger); */
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      /* console.log('this product form', thisProduct.form); */
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      /* console.log('this product form inputs', thisProduct.formInputs); */
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      /* console.log('this product cart button', thisProduct.cartButton); */
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      /* console.log('this product price elem', thisProduct.priceElem); */
+    }
+
     initAccordion(){
       const thisProduct = this;
 
@@ -99,8 +118,59 @@
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
-      }
     }
+
+    initOrderForm(){
+      const thisProduct = this;
+
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+    }
+
+    processOrder() {
+      const thisProduct = this;
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+    }
+
+  }
 
 
   const app = {
