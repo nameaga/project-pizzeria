cc022f96e99edf4629389be345ba832cbedbe6ae
Agnieszka Wenerska <wenerskaaga@gmail.com>
2022-10-25 02:19:24 +0200

    Add cartProducts remove option

--- src/js/script.js
+++ src/js/script.js
@@ -401,6 +401,9 @@
       thisCart.dom.productList.addEventListener('updated', function(){
         thisCart.update();
       });
+      thisCart.dom.productList.addEventListener('remove', function(event){
+        thisCart.remove(event.detail.cartProduct);
+      });
     }
     add(menuProduct){
       const thisCart = this;
@@ -445,6 +448,15 @@
         price.innerHTML = thisCart.totalPrice;
       }
     }
+    remove(event){
+      const thisCart = this;
+
+      event.dom.wrapper.remove();
+
+      const productsRemove = thisCart.products.indexOf(event);
+      thisCart.products.splice(productsRemove, 1);
+      thisCart.update();
+    }
   }
   class CartProduct{
     constructor(menuProduct, element){
@@ -458,6 +470,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
       //console.log('cart', thisCartProduct);
     }
     getElements(element) {
@@ -480,8 +493,31 @@
         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       });
-  }
+    }
+    remove() {
+      const thisCartProduct = this;
 
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+    initActions() {
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function (event) {
+        event.preventDefault();
+      });
+      thisCartProduct.dom.remove.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisCartProduct.remove();
+
+      });
+    }
   }
 
   const app = {
